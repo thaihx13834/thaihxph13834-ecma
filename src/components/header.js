f89525b073df24c001b30c524952e03fb7793a21
anhhv1234 <93896412+thaihx13834@users.noreply.github.com>
2022-01-15 12:55:54 +0700

    fx

--- src/components/header.js
+++ src/components/header.js
@@ -10,8 +10,8 @@ const Header = {
               <li><a href="/">Trang chủ</a></li>
               <li><a href="/news">News</a></li>
               <li><a href="/signin">Chuong trình đào tạo</a></li>
-              <li><a href="">Góc sinh viên</a></li>
-              <li><a href="">Tuyển dụng</a></li>
+              <li><a href="/signup">Góc sinh viên</a></li>
+              <li><a href="/admin/dashboard">Tuyển dụng</a></li>
             </ul>
           </div>
 
