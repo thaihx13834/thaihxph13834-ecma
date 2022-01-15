826e1a57ac601a5ee60e1f5e0473665055f6121c
anhhv1234 <93896412+thaihx13834@users.noreply.github.com>
2022-01-15 12:58:01 +0700

    s

--- src/pages/client/signup.js
+++ src/pages/client/signup.js
@@ -48,7 +48,7 @@ const Signup = {
                     placeholder="Mật khẩu" type="password" />
                 </div>
                 <button
-                  class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-blue-600 border border-transparent rounded-lg active:bg-blue-600 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue"
+                  class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150  bg-orange-600 hover:bg-orange-700 border border-transparent rounded-lg active:bg-orange-600 focus:outline-none focus:shadow-outline-blue"
                   href="#">
                   Đăng kí
                 </button>
